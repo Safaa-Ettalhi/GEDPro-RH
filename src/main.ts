63a9ca0be20cc85b227811047e788ff3ad5e0e26
Safaa <Safaeettalhi@gmail.com>
2025-12-25 16:19:00 +0100

    activation de la validation automatique et du CORS dans l'application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //avtivation de validation automatique
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`Application démarrée sur http://localhost:${port}`);
 }
 bootstrap();
