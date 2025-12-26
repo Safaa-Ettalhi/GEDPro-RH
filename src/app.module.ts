dd7c74ac186d0c3e062e6bf68d58ee2d5843e3ea
Safaa <Safaeettalhi@gmail.com>
2025-12-26 16:37:02 +0100

    branchement de l’authentification dans l’application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -20,6 +20,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot(
       process.env.MONGO_URI || 'mongodb://localhost:27017/GEDPRO',
     ),
+    AuthModule,
   ],
 })
 export class AppModule {}
