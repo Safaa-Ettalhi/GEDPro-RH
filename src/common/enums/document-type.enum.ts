d173d5f48c6e68e6074d43a2837cde7ef0bf83fd
Safaa <Safaeettalhi@gmail.com>
2025-12-30 14:15:08 +0100

    Ajout de l'enum DocumentType pour gérer les types de documents

---
+++ src/common/enums/document-type.enum.ts
@@ -0,0 +1,10 @@
+export enum DocumentType {
+  CV = 'cv',
+  COVER_LETTER = 'cover_letter',
+  CONTRACT = 'contract',
+  DIPLOMA = 'diploma',
+  CERTIFICATE = 'certificate',
+  IDENTITY = 'identity',
+  EVALUATION = 'evaluation',
+  OTHER = 'other',
+}
