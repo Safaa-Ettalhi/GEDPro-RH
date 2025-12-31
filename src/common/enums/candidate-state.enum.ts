daf8727f9c6e6f59760df0d961c261d3f0b73a12
Safaa <Safaeettalhi@gmail.com>
2025-12-31 11:56:09 +0100

    Création de l’enum CandidateState pour définir les différents états d’un candidat

---
+++ src/common/enums/candidate-state.enum.ts
@@ -0,0 +1,8 @@
+export enum CandidateState {
+  NOUVEAU = 'nouveau',
+  PRESELECTIONNE = 'preselectionne',
+  ENTRETIEN_PLANIFIE = 'entretien_planifie',
+  EN_ENTRETIEN = 'en_entretien',
+  ACCEPTE = 'accepte',
+  REFUSE = 'refuse',
+}
