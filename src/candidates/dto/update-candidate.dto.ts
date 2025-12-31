2d2f399d490624a59cccfa1fb49b785e18f4ca46
Safaa <Safaeettalhi@gmail.com>
2025-12-31 11:37:19 +0100

    Création de UpdateCandidateDto pour permettre la mise à jour des candidats avec champs optionnels et validations associées

---
+++ src/candidates/dto/update-candidate.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsInt,
+  MinLength,
+} from 'class-validator';
+
+export class UpdateCandidateDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  lastName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsInt()
+  jobOfferId?: number;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+}
