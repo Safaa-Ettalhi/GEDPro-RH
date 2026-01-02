865977e0db8b853034a2a60ff2db121033d835ed
Safaa <Safaeettalhi@gmail.com>
2026-01-02 20:45:28 +0100

    configuration de web socket

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SocketIOAdapter } from './common/adapters/socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new SocketIOAdapter(app));
 
-  // Activation de validation automatique
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,10 +15,15 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   console.log(`Application démarrée sur http://localhost:${port}`);
+  console.log(`WebSocket disponible sur ws://localhost:${port}/notifications`);
 }
 bootstrap();
