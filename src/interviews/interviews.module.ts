19df730f54b27ffb2fac10e0379670edec7f3d51
Safaa <Safaeettalhi@gmail.com>
2026-01-02 13:57:33 +0100

    integration de google calendar

--- src/interviews/interviews.module.ts
+++ src/interviews/interviews.module.ts
@@ -8,6 +8,7 @@ import { Organization } from '../organizations/entities/organization.entity';
 import { UserOrganization } from '../organizations/entities/user-organization.entity';
 import { User } from '../auth/entities/user.entity';
 import { CandidatesModule } from '../candidates/candidates.module';
+import { GoogleCalendarService } from './services/google-calendar.service';
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { CandidatesModule } from '../candidates/candidates.module';
     CandidatesModule,
   ],
   controllers: [InterviewsController],
-  providers: [InterviewsService],
+  providers: [InterviewsService, GoogleCalendarService],
   exports: [InterviewsService],
 })
 export class InterviewsModule {}
