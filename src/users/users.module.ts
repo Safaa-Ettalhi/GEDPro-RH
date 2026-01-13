3b594ede8ec09fe9c9dc170ceba77b3033ec03f3
Safaa <Safaeettalhi@gmail.com>
2026-01-13 09:59:11 +0100

    ajoute les entités UserOrganization et Organization au module Users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from '../auth/entities/user.entity';
+import { UserOrganization } from '../organizations/entities/user-organization.entity';
+import { Organization } from '../organizations/entities/organization.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserOrganization, Organization])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
