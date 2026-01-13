eaeb442a5e7f65bbf787af3a0545e71513a342ed
Safaa <Safaeettalhi@gmail.com>
2026-01-13 14:26:33 +0100

    ajoute CandidateDocument au module TypeOrm pour la gestion des documents

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -5,13 +5,19 @@ import { DocumentsController } from './documents.controller';
 import { Document } from './entities/document.entity';
 import { Organization } from '../organizations/entities/organization.entity';
 import { UserOrganization } from '../organizations/entities/user-organization.entity';
+import { CandidateDocument } from '../candidates/entities/candidate-document.entity';
 import { MinioService } from './services/minio.service';
 import { SkillsModule } from '../skills/skills.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Document, Organization, UserOrganization]),
+    TypeOrmModule.forFeature([
+      Document,
+      Organization,
+      UserOrganization,
+      CandidateDocument,
+    ]),
     SkillsModule,
     NotificationsModule,
   ],
